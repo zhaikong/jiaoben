de85b845396d53e6b9f2989eb5ee7eb201ff0d2c
zhaikong <88657496+zhaikong@users.noreply.github.com>
2024-10-17 13:23:51 +0800

    Update kj.js

--- kj.js
+++ kj.js
@@ -8,10 +8,10 @@
 更新时间：2024-2-3
 问题反馈：
 使用声明：⚠️此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！⚠️⚠️⚠️
-*******************************
+******************************
 [rewrite_local]
 # > 芬空间,会员课+付费课+大师课+直播课
-^https?:\/\/api\.zhangdefenspace\.com\/api\/(course\/getCoursePeriodList|course\/getCourseDetails|user\/getIndexUser|v2\/live\/getLiveDetails).*$ url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/Fkj.js
+^https?:\/\/api\.zhangdefenspace\.com\/api\/(course\/getCoursePeriodList|course\/getCourseDetails|user\/getIndexUser|v2\/live\/getLiveDetails).*$ url script-response-body https://raw.githubusercontent.com/zhaikong/jiaoben/main/kj.js
 
 [mitm] 
 hostname = api.zhangdefenspace.com
@@ -21,30 +21,31 @@ hostname = api.zhangdefenspace.com
 *
 */
 
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
+let body = $response.body;
+let url = $request.url;
+let obj = JSON.parse(body);
 
-const vip = '/user/getIndexUser';
-const course = '/course/getCourseDetails';
-const coursePeriod = '/course/getCoursePeriodList';
-const live = '/v2/live/getLiveDetails';
+const vipPattern = /\/user\/getIndexUser/;
+const coursePattern = /\/(course\/getCourseDetails|course\/getCoursePeriodList|v2\/live\/getLiveDetails)/;
 
-if (url.indexOf(vip) != -1) {
-    obj.data.vipInfo.isVip = true;
-    obj.data.vipInfo.vipEndTime = "2099-12-31 23:59:59";
-    obj.data.vipInfo.vipLevel = 3;
-} else if (url.indexOf(course) != -1 || url.indexOf(coursePeriod) != -1 || url.indexOf(live) != -1) {
-    if (obj.data.hasOwnProperty('isBuy')) {
-        obj.data.isBuy = true;
+try {
+    if (vipPattern.test(url)) {
+        if (obj.data && obj.data.vipInfo) {
+            obj.data.vipInfo.isVip = true;
+            obj.data.vipInfo.vipEndTime = "2099-12-31 23:59:59";
+            obj.data.vipInfo.vipLevel = 3;
+        }
+    } else if (coursePattern.test(url)) {
+        if (obj.data) {
+            obj.data.isBuy = true;
+            obj.data.isVip = true;
+            obj.data.canWatch = true;
+        }
     }
-    if (obj.data.hasOwnProperty('isVip')) {
-        obj.data.isVip = true;
-    }
-    if (obj.data.hasOwnProperty('canWatch')) {
-        obj.data.canWatch = true;
-    }
+
+    body = JSON.stringify(obj);
+} catch (err) {
+    console.log('芬空间解锁脚本运行出错：' + err);
 }
 
-body = JSON.stringify(obj);
 $done({body});
