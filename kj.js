549a9095f61e0f8ab0d68a38cc4ba1783ff17a3c
zhaikong <88657496+zhaikong@users.noreply.github.com>
2024-10-17 13:37:44 +0800

    Update kj.js

--- kj.js
+++ kj.js
@@ -21,59 +21,50 @@ hostname = api.zhangdefenspace.com
 *
 */
 
-let body = $response.body;
-let url = $request.url;
-let obj = JSON.parse(body);
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
 
-const vipPattern = /\/user\/getIndexUser/;
-const coursePattern = /\/(course\/getCourseDetails|course\/getCoursePeriodList|v2\/live\/getLiveDetails)/;
+const vip = '/user/getIndexUser';
+const course = '/course/getCourseDetails';
+const coursePeriod = '/course/getCoursePeriodList';
+const live = '/v2/live/getLiveDetails';
 
-try {
-    if (vipPattern.test(url)) {
-        if (obj.data) {
-            obj.data.isVip = true;
-            obj.data.vipLevel = 3;
-            obj.data.vipEndTime = "2099-12-31 23:59:59";
-            if (obj.data.vipInfo) {
-                obj.data.vipInfo.isVip = true;
-                obj.data.vipInfo.vipEndTime = "2099-12-31 23:59:59";
-                obj.data.vipInfo.vipLevel = 3;
-            }
-        }
-    } else if (coursePattern.test(url)) {
-        if (obj.data) {
-            obj.data.isBuy = true;
-            obj.data.isVip = true;
-            obj.data.canWatch = true;
-            obj.data.isFree = true;
-            obj.data.isUnlock = true;
-            obj.data.isLock = false;
-            
-            if (obj.data.videoInfo) {
-                obj.data.videoInfo.canWatch = true;
-                obj.data.videoInfo.isLock = false;
-                obj.data.videoInfo.isBuy = true;
-            }
-            
-            if (obj.data.periodList && Array.isArray(obj.data.periodList)) {
-                obj.data.periodList.forEach(period => {
-                    period.isBuy = true;
-                    period.canWatch = true;
-                    period.isLock = false;
-                    period.isFree = true;
-                    if (period.videoInfo) {
-                        period.videoInfo.canWatch = true;
-                        period.videoInfo.isLock = false;
-                        period.videoInfo.isBuy = true;
-                    }
-                });
-            }
-        }
+if (url.indexOf(vip) != -1) {
+    obj.data.vipInfo.isVip = true;
+    obj.data.vipInfo.vipEndTime = "2099-12-31 23:59:59";
+    obj.data.vipInfo.vipLevel = 3;
+    obj.data.isVip = true;
+    obj.data.vipLevel = 3;
+    obj.data.vipEndTime = "2099-12-31 23:59:59";
+} else if (url.indexOf(course) != -1 || url.indexOf(coursePeriod) != -1 || url.indexOf(live) != -1) {
+    obj.data.isBuy = true;
+    obj.data.isVip = true;
+    obj.data.canWatch = true;
+    obj.data.isFree = true;
+    obj.data.isUnlock = true;
+    obj.data.isLock = false;
+    
+    if (obj.data.videoInfo) {
+        obj.data.videoInfo.canWatch = true;
+        obj.data.videoInfo.isLock = false;
+        obj.data.videoInfo.isBuy = true;
     }
-
-    body = JSON.stringify(obj);
-} catch (err) {
-    console.log('芬空间解锁脚本运行出错：' + err);
+    
+    if (obj.data.periodList && Array.isArray(obj.data.periodList)) {
+        obj.data.periodList.forEach(period => {
+            period.isBuy = true;
+            period.canWatch = true;
+            period.isLock = false;
+            period.isFree = true;
+            if (period.videoInfo) {
+                period.videoInfo.canWatch = true;
+                period.videoInfo.isLock = false;
+                period.videoInfo.isBuy = true;
+            }
+        });
+    }
 }
 
+body = JSON.stringify(obj);
 $done({body});
